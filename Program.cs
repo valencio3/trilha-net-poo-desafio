62621237428870a941d1c576f3160f207a35238e
Raphael Valencio <ph.valencio@gmail.com>
2023-11-25 15:01:33 -0400

    test: Add test cases for Nokia and Iphone classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+var nokia = new Nokia("123456","2120","123456789", 2);
+Console.WriteLine("-------- testando celular nokia --------");
+nokia.Ligar();
+nokia.ReceberLigacao();
+Console.WriteLine($"O número do celular é {nokia.Numero}");
+nokia.Numero = "654321";
+Console.WriteLine($"O novo número do celular é {nokia.Numero}");
+
+var iphone = new Iphone("123456","2120","123456789", 2);
+Console.WriteLine("-------- testando celular iphone --------");
+Console.WriteLine($"O número do celular é {iphone.Numero}");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.Numero = "654321";
+Console.WriteLine($"O novo número do celular é {iphone.Numero}");

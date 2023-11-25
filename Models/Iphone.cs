d7cbb574449c51ac86254709282b915987be4fc2
Raphael Valencio <ph.valencio@gmail.com>
2023-11-25 14:50:37 -0400

    feat: Implement the Iphone class

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,5 @@
-namespace DesafioPOO.Models
+namespace DesafioPOO.Models;
+public class Iphone(string numero, string modelo, string imei, int memoria) : Smartphone(numero, modelo, imei, memoria)
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
-    {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
-    }
+	public override void InstalarAplicativo(string nomeApp) => Console.WriteLine($"Instalnado o aplicativo {nomeApp}");
 }
